1d04a4dae4fdee4f461bda479a2b247aabea4d2e
alessandro-barate <161752327+aleek89@users.noreply.github.com>
2026-04-14 02:06:08 +0200

    set up

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -2111,7 +2111,22 @@ const plugins = [
   _2fMp1pUHyZaUu6ZVG2fKPFET2utk9oPq4mFJPGf3qw
 ];
 
-const assets = {};
+const assets = {
+  "/index.mjs": {
+    "type": "text/javascript; charset=utf-8",
+    "etag": "\"1a014-1EbYJ2aN9FmdcBf9pOMysGXwZMI\"",
+    "mtime": "2026-04-13T23:42:32.760Z",
+    "size": 106516,
+    "path": "index.mjs"
+  },
+  "/index.mjs.map": {
+    "type": "application/json",
+    "etag": "\"69562-do7d64AyBKgspKkbbUDtnn4p81U\"",
+    "mtime": "2026-04-13T23:42:32.760Z",
+    "size": 431458,
+    "path": "index.mjs.map"
+  }
+};
 
 function readAsset (id) {
   const serverDir = dirname$1(fileURLToPath(globalThis._importMeta_.url));
